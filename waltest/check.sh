af6e532a80fd7b66fac006bfb7e5e728cc8adf09
tom--bo <otsuka.t.2013@gmail.com>
2016-05-20 09:21:08 +0000

    Add script for wal test

---
+++ waltest/check.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+CLUSTER=slony_example
+DBNAME1=pgbench
+DBNAME2=pgbench
+HOST1=192.168.33.101
+HOST2=192.168.33.102
+SLONY_USER=pgsql
+PGBENCH_USER=pgbench
+
+echo -n "**** comparing sample1 ... "
+psql -U $PGBENCH_USER -h $HOST1 $DBNAME1 >dump.tmp.1.$$ <<_EOF_
+    select 'accounts:'::text, aid, bid, abalance, filler
+        from pgbench_accounts order by aid;
+    select 'branches:'::text, bid, bbalance, filler
+        from pgbench_branches order by bid;
+    select 'tellers:'::text, tid, bid, tbalance, filler
+        from pgbench_tellers order by tid;
+    select 'history:'::text, tid, bid, aid, delta, mtime, filler,
+        id
+        from pgbench_history order by id;
+_EOF_
+
+
+psql -U $PGBENCH_USER -h $HOST2 $DBNAME2 >dump.tmp.2.$$ <<_EOF_
+    select 'accounts:'::text, aid, bid, abalance, filler
+        from pgbench_accounts order by aid;
+    select 'branches:'::text, bid, bbalance, filler
+        from pgbench_branches order by bid;
+    select 'tellers:'::text, tid, bid, tbalance, filler
+        from pgbench_tellers order by tid;
+    select 'history:'::text, tid, bid, aid, delta, mtime, filler,
+        id
+        from pgbench_history order by id;
+_EOF_
+
+if diff dump.tmp.1.$$ dump.tmp.2.$$ >test_1.diff ; then
+    echo "success - databases are equal."
+    rm dump.tmp.?.$$
+    rm test_1.diff
+else
+    echo "FAILED - see test_1.diff for database differences"
+fi
+
+# If this script reports any differences, it is worth reporting this to
+# the developers as we would appreciate hearing how this happened.
---
+++ waltest/repli_start.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+CLUSTER=slony_example
+DBNAME1=pgbench
+DBNAME2=pgbench
+HOST1=192.168.32.31
+HOST2=192.168.32.32
+SLONY_USER=pgsql
+PGBENCH_USER=pgbench
+
+slonik <<_EOF_
+    # ----
+    # This defines which namespace the replication system uses
+    # ----
+    cluster name = $CLUSTER;
+
+    # ----
+    # Admin conninfo's are used by the slonik program to connect
+    # to the node databases.  So these are the PQconnectdb arguments
+    # that connect from the administrators workstation (where
+    # slonik is executed).
+    # ----
+    node 1 admin conninfo = 'dbname=$DBNAME1 host=$HOST1 user=$SLONY_USER';
+    node 2 admin conninfo = 'dbname=$DBNAME2 host=$HOST2 user=$SLONY_USER';
+
+    # ----
+    # Node 2 subscribes set 1
+    # ----
+    subscribe set ( id = 1, provider = 1, receiver = 2, forward = yes);
+_EOF_
+
