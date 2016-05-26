0a22511ed4b423dfc70a4affa944414c36b470b9
postgresql <postgres@debian-8.4-amd64>
2016-05-26 12:10:17 +0000

    switchover

---
+++ waltest/switch_over.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CLUSTER=slony_example
+DBNAME1=pgbench
+DBNAME2=pgbench
+DBNAME3=pgbench
+HOST1=192.168.32.101
+HOST2=192.168.32.103
+SLONY_USER=pgsql
+
+slonik <<_EOF_
+    
+    cluster name = $CLUSTER;
+
+    node 1 admin conninfo = 'dbname=$DBNAME1 host=$HOST1 user=$SLONY_USER';
+    node 2 admin conninfo = 'dbname=$DBNAME2 host=$HOST2 user=$SLONY_USER';
+
+    lock set (id = 1, origin = 1);
+    sync (id = 1);
+    wait for event (origin = 1, confirmed = 2, wait on = 2);
+    move set (id = 1, old origin = 1, new origin = 2);
+    echo 'moved the set';
+    wait for event (origin = 1, confirmed = 2, wait on = 1);
+
+_EOF_
+
